de51f660d4377a1272a6d1959120f25e6c96fc6c
rrzt <124551348+h465855hgg@users.noreply.github.com>
2026-01-19 18:10:01 +0800

    null

--- app/src/main/java/com/web/webide/ui/editor/components/SearchPanel.kt
+++ app/src/main/java/com/web/webide/ui/editor/components/SearchPanel.kt
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
 package com.web.webide.ui.editor.components
 
 import androidx.compose.animation.AnimatedVisibility
